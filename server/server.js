df94341c00c5c5a3c164e34f211c152d34697fb1
anirudhVASUDEV23 <anirudh.vasudev.kesana@gmail.com>
2025-04-09 13:16:03 +0530

    Added health check route at root url

--- server/server.js
+++ server/server.js
@@ -1,24 +1,4 @@
-import express from "express";
-import cors from "cors";
-import { connectDB } from "./db/connection.js";
-import { PORT } from "./config.js";
-import authRoutes from "./routes/authRoutes.js";
-import linkRoutes from "./routes/linkRoutes.js";
-import User from "./models/User.js";
-
-// Create Express app
-const app = express();
-
-// Connect to MongoDB
-connectDB();
-
-// Middleware
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true }));
-
-// API Routes
-import express from "express";
+// API Route import express from "express";
 import cors from "cors";
 import { connectDB } from "./db/connection.js";
 import { PORT } from "./config.js";
